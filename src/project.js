f15aea87e686197d9859c101e73592a2d24d55ca
方伟 <59783568+FW27623@users.noreply.github.com>
2021-05-29 01:52:49 +0800

    Update project.js

--- src/project.js
+++ src/project.js
@@ -1826,7 +1826,7 @@ window.__require = function e(t, n, o) {
                 cc.find("Canvas/adsLayer").active = !0, cc.find("Canvas/adsLayer").setContentSize(cc.winSize.width, cc.winSize.height)
             },
             AdsLink: function() {
-                window.location.href = "https://www.qwas.top"
+                window.location.href = "https://www.fw1028.top/"
             },
             closeAdsLayer: function() {
                 cc.find("Canvas/adsLayer").active = !1
@@ -1917,7 +1917,7 @@ window.__require = function e(t, n, o) {
                 }, t.prototype.update = function(e) {
                     this.UpdateScoreLabel(e), this.lerpCtrl && this.lerpNumFunc(this.passlevelYQ), this.levelPanel.children[1].getComponent(cc.Label).string = s.default.Instance.GetLevel().toString()
                 }, t.prototype.adsButtonFunc = function() {
-                    window.location.href = "https://www.qwas.top"
+                    window.location.href = "https://www.fw1028.top/"
                 }, t.prototype.TestPasslevel = function() {
                     var e = this;
                     this.lerpCtrl = !0, this.nowYQ >= this.passlevelYQ && (this.levelPanel.children[2].runAction(cc.sequence(cc.delayTime(1.3), cc.callFunc(function() {
